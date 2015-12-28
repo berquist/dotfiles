2ad93218285faa4cfd1aa6dcd577eea373dba87f
Eric Berquist <eric.berquist@gmail.com>
2015-12-28 17:46:05 -0500

    emacs: Add properties to each block, and move custom-set-variables.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,3 +38,64 @@
 ;;; Load configuration
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; M-x customize
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   [(\, wombat-bg)
+    (\, wombat-red)
+    (\, wombat-green)
+    (\, wombat-green+1)
+    (\, wombat-blue)
+    "#333366" "#ccaa8f"
+    (\, wombat-fg)])
+ '(browse-url-browser-function (quote browse-url-firefox))
+ '(c-basic-offset 2)
+ '(c-default-style
+   (quote
+    ((java-mode . "java")
+     (awk-mode . "awk")
+     (other . "k&r"))))
+ '(c-doc-comment-style
+   (quote
+    ((c-mode . javadoc)
+     (java-mode . javadoc)
+     (pike-mode . autodoc))))
+ '(custom-safe-themes
+   (quote
+    ("6eaebdc2426b0edfff9fd9a7610f2fe7ddc70e01ceb869aaaf88b5ebe326a0cd" "2d7e4feac4eeef3f0610bf6b155f613f372b056a2caae30a361947eab5074716" default)))
+ '(dtrt-indent-mode t nil (dtrt-indent))
+ '(fortran-comment-region "cccc")
+ '(indicate-buffer-boundaries (quote right))
+ '(markdown-coding-system (quote utf-8))
+ '(markdown-command
+   "pandoc -c file://${HOME}/.emacs.d/github-pandoc.css -f markdown_github -t html5 --smart --mathjax --highlight-style pygments --standalone")
+ '(markdown-css-path "file://${HOME}/.emacs.d/github-pandoc.css")
+ '(markdown-enable-math t)
+ '(markdown-link-space-sub-char "-")
+ '(markdown-preview-style "file://${HOME}/.emacs.d/github-markdown.css")
+ '(my-global-rainbow-mode nil)
+ '(paren-delay nil)
+ '(paren-highlight-at-point t)
+ '(paren-highlight-offscreen t)
+ '(preview-scale-function 1.0)
+ '(py-fontify-shell-buffer-p t)
+ '(vc-follow-symlinks t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(trailing-whitespace ((t (:background "#e57890"))))
+ '(whitespace-empty ((t (:background "firebrick4" :foreground "firebrick"))))
+ '(whitespace-newline ((t (:foreground "#99968b" :weight normal))))
+ '(whitespace-space ((t (:background "#303030" :foreground "#424242"))))
+ '(whitespace-tab ((t (:background "#303030" :foreground "#424242")))))
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
