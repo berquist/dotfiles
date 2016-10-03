eab6bb79d5edeec6f925957a10683b8979b3245a
Eric Berquist <eric.berquist@gmail.com>
2016-10-03 08:17:44 -0400

    resolve merge conflict

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,9 +78,10 @@
  '(markdown-link-space-sub-char "-")
  '(markdown-preview-style "file://${HOME}/.emacs.d/github-markdown.css")
  '(my-global-rainbow-mode nil)
+ '(org-closed-keep-when-no-todo t)
  '(package-selected-packages
    (quote
-    (yaml-mode use-package pkgbuild-mode pandoc-mode ox-twbs ox-pandoc ox-gfm mic-paren matlab-mode markdown-mode julia-mode htmlize flycheck-pyflakes flycheck-cython flycheck-checkbashisms editorconfig dtrt-indent deft cython-mode cuda-mode cmake-mode chapel-mode)))
+    (yaml-mode use-package scratch rainbow-mode pkgbuild-mode pandoc-mode ox-twbs ox-pandoc ox-gfm mic-paren matlab-mode markdown-mode julia-mode htmlize gitignore-mode gitconfig-mode flycheck-pyflakes flycheck-cython flycheck-checkbashisms exec-path-from-shell editorconfig dtrt-indent deft cython-mode cuda-mode cmake-mode circe chapel-mode)))
  '(paren-delay nil)
  '(paren-highlight-at-point t)
  '(paren-highlight-offscreen t)
