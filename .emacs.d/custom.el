b3d4e4662adabe405a082afa3f1341c2885d3a48
Eric Berquist <eric.berquist@gmail.com>
2017-10-03 19:44:42 -0400

    [emacs] start writing Scheme (Racket) again

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,6 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-firefox))
  '(completions-format (quote vertical))
+ '(custom-safe-themes
+   (quote
+    ("2a14752b82660cf074d5e910fdd6933b1df2ec992a00e38aa53c9471a9f823dc" default)))
  '(indicate-buffer-boundaries (quote right))
  '(magit-commit-arguments (quote ("--verbose")))
  '(octave-block-offset 4)
@@ -32,7 +35,7 @@
      ("colorlinks=true" "hyperref" nil))))
  '(package-selected-packages
    (quote
-    (flycheck-cython flycheck-pyflakes yaml-mode wc-mode use-package unfill systemd rust-mode ripgrep pkgbuild-mode pandoc-mode ox-pandoc ox-gfm mic-paren markdown-mode magit-svn lua-mode julia-mode json-mode htmlize gitignore-mode gitconfig-mode flycheck-checkbashisms exec-path-from-shell editorconfig dtrt-indent deft cython-mode cuda-mode cmake-mode clang-format chapel-mode anzu)))
+    (geiser rainbow-mode racket-mode ahungry-theme edit-indirect flycheck-cython flycheck-pyflakes yaml-mode wc-mode use-package unfill systemd rust-mode ripgrep pkgbuild-mode pandoc-mode ox-pandoc ox-gfm mic-paren markdown-mode magit-svn lua-mode julia-mode json-mode htmlize gitignore-mode gitconfig-mode flycheck-checkbashisms exec-path-from-shell editorconfig dtrt-indent deft cython-mode cuda-mode cmake-mode clang-format chapel-mode anzu)))
  '(pandoc-major-modes
    (quote
     ((text-mode . "markdown")
@@ -56,4 +59,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(racket-selfeval-face ((t (:foreground "#4bc98a")))))
