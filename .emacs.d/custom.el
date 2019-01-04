27ac751bdd1946548795cb3fac025620cc319987
Eric Berquist <berquist@isi.edu>
2019-01-04 11:51:23 -0500

    [emacs] default to trying eww for a browser

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-firefox))
  '(completions-format (quote vertical))
  '(custom-safe-themes
    (quote
