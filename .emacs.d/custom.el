a41fee0a68a72baff4aa19223a76ba889cea8c9c
Eric Berquist <berquist@isi.edu>
2019-06-20 12:25:00 -0400

    [emacs] general updates

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(completions-format (quote vertical))
  '(custom-safe-themes
    (quote
     ("2a14752b82660cf074d5e910fdd6933b1df2ec992a00e38aa53c9471a9f823dc" default)))
@@ -12,7 +11,6 @@
     ("display" "displaymath" "equation" "eqnarray" "gather" "math" "multline" "align" "alignat" "xalignat" "xxalignat" "flalign" "dmath")))
  '(nxml-child-indent 4)
  '(nxml-outline-child-indent 4)
- '(octave-block-offset 4 t)
  '(org-latex-default-packages-alist
    (quote
     (("AUTO" "inputenc" t
@@ -30,6 +28,9 @@
      ("" "amssymb" t)
      ("" "capt-of" nil)
      ("colorlinks=true" "hyperref" nil))))
+ '(package-selected-packages
+   (quote
+    (clojure-mode yaml-mode web-mode web-beautify wc-mode use-package unfill typescript-mode tuareg ttl-mode systemd swift-mode strace-mode ssass-mode sphinx-mode sparql-mode slime scala-mode ripgrep rebecca-theme racket-mode quasi-monochrome-theme pydoc-info pydoc punpun-theme plan9-theme pkgbuild-mode pipenv pip-requirements pandoc-mode ox-pandoc ox-gfm nim-mode monochrome-theme mmm-mode mic-paren mediawiki magit-svn magit-popup lush-theme lua-mode lsp-ui lsp-sh lsp-rust lsp-python lsp-java lsp-clangd lorem-ipsum langtool kotlin-mode julia-mode json-mode jinja2-mode hy-mode htmlize highlight graphviz-dot-mode graphql gotham-theme go-mode gitignore-mode gitconfig-mode gitattributes-mode ghub geiser flycheck-yamllint flycheck-vale flycheck-rust flycheck-pyre flycheck-pyflakes flycheck-pycheckers flycheck-mypy flycheck-kotlin flycheck-julia flycheck-cython flycheck-checkbashisms exec-path-from-shell elpy elcord eglot editorconfig edit-indirect dtrt-indent dracula-theme dockerfile-mode delight deft cython-mode cuda-mode csharp-mode crystal-mode company-lsp company-anaconda coffee-mode cmake-mode clang-format chapel-mode cargo auctex-latexmk anzu aggressive-indent abyss-theme 0blayout)))
  '(preview-scale-function 1.0)
  '(py-fontify-shell-buffer-p t)
  '(safe-local-variable-values
