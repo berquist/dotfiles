43ff0e551a965804c02f946314020d8606aa8163
Eric Berquist <berquist@isi.edu>
2019-06-29 14:02:35 -0400

    [emacs] not sure how these things ended up in custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,8 +9,6 @@
  '(font-latex-math-environments
    (quote
     ("display" "displaymath" "equation" "eqnarray" "gather" "math" "multline" "align" "alignat" "xalignat" "xxalignat" "flalign" "dmath")))
- '(nxml-child-indent 4)
- '(nxml-outline-child-indent 4)
  '(org-latex-default-packages-alist
    (quote
     (("AUTO" "inputenc" t
@@ -28,10 +26,6 @@
      ("" "amssymb" t)
      ("" "capt-of" nil)
      ("colorlinks=true" "hyperref" nil))))
- '(package-selected-packages
-   (quote
-    (clojure-mode yaml-mode web-mode web-beautify wc-mode use-package unfill typescript-mode tuareg ttl-mode systemd swift-mode strace-mode ssass-mode sphinx-mode sparql-mode slime scala-mode ripgrep rebecca-theme racket-mode quasi-monochrome-theme pydoc-info pydoc punpun-theme plan9-theme pkgbuild-mode pipenv pip-requirements pandoc-mode ox-pandoc ox-gfm nim-mode monochrome-theme mmm-mode mic-paren mediawiki magit-svn magit-popup lush-theme lua-mode lsp-ui lsp-sh lsp-rust lsp-python lsp-java lsp-clangd lorem-ipsum langtool kotlin-mode julia-mode json-mode jinja2-mode hy-mode htmlize highlight graphviz-dot-mode graphql gotham-theme go-mode gitignore-mode gitconfig-mode gitattributes-mode ghub geiser flycheck-yamllint flycheck-vale flycheck-rust flycheck-pyre flycheck-pyflakes flycheck-pycheckers flycheck-mypy flycheck-kotlin flycheck-julia flycheck-cython flycheck-checkbashisms exec-path-from-shell elpy elcord eglot editorconfig edit-indirect dtrt-indent dracula-theme dockerfile-mode delight deft cython-mode cuda-mode csharp-mode crystal-mode company-lsp company-anaconda coffee-mode cmake-mode clang-format chapel-mode cargo auctex-latexmk anzu aggressive-indent abyss-theme 0blayout)))
- '(preview-scale-function 1.0)
  '(py-fontify-shell-buffer-p t)
  '(safe-local-variable-values
    (quote
@@ -43,10 +37,7 @@
      (eval progn
            (flycheck-mode nil))
      (eval wc-mode t)
-     (dtrt-indent-mode . 0))))
- '(save-interprogram-paste-before-kill t)
- '(tabbar-separator (quote (0.5)))
- '(tabbar-use-images nil))
+     (dtrt-indent-mode . 0)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
