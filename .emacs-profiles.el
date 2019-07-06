022075c6b57e203ee6af244be63b277f3b6366d4
Eric Berquist <eric.berquist@gmail.com>
2019-07-06 16:19:06 -0400

    [emacs] profile picker for chemacs

---
+++ .emacs-profiles.el
@@ -0,0 +1,10 @@
+;;;
+
+;;; Commentary:
+
+;;; Code:
+
+(("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("fresh" . ((user-emacs-directory . "~/.emacs.d_fresh"))))
+
+;;; .emacs-profiles.el ends here
