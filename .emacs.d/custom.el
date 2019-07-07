32a09cd051ff0a41a7cf1329854b7a4242d8bd6e
Eric Berquist <eric.berquist@gmail.com>
2019-07-07 13:35:41 -0400

    [emacs] temporarily add some Company faces to custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -36,4 +36,17 @@
      (eval progn
            (flycheck-mode nil))
      (eval wc-mode t)
-     (dtrt-indent-mode . 0)))))
+     (dtrt-indent-mode . 0))))
+ '(wakatime-python-bin nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-preview ((t (:background "midnight blue" :foreground "wheat"))))
+ '(company-preview-common ((t (:inherit company-preview :foreground "salmon"))))
+ '(company-scrollbar-bg ((t (:background "chocolate"))))
+ '(company-scrollbar-fg ((t (:background "IndianRed4"))))
+ '(company-template-field ((t (:background "light sea green" :foreground "white"))))
+ '(company-tooltip ((t (:background "dark slate gray" :foreground "white"))))
+ '(company-tooltip-selection ((t (:background "DarkOrange3")))))
