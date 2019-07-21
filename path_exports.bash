ff9e99fba6f48c9fe4e72338550a8e30f84364b6
Eric Berquist <eric.berquist@gmail.com>
2019-07-21 16:54:24 -0400

    [shell] add Exercism workspace (don't think this works)

--- path_exports.bash
+++ path_exports.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export EXERCISM_WORKSPACE="${HOME}"/development/exercism
+export npm_config_prefix="${HOME}"/.node_modules
+export PIP_CONFIG_FILE="${HOME}"/dotfiles/pip.conf
+
 pythonpathdirs=($(find "${HOME}"/pythonpath -maxdepth 1 -type d))
 # https://stackoverflow.com/questions/1335815/how-to-slice-an-array-in-bash
 for d in ${pythonpathdirs[@]:1}; do
@@ -7,8 +11,6 @@ for d in ${pythonpathdirs[@]:1}; do
 done
 
 export LD_LIBRARY_PATH="${HOME}"/opt/lib:"${LD_LIBRARY_PATH}"
-
-export PIP_CONFIG_FILE="${HOME}"/dotfiles/pip.conf
 
 [[ -d "${HOME}"/personal_scripts ]] && export PATH="${HOME}"/personal_scripts:"${PATH}"
 [[ -d "${HOME}"/opt/bin ]] && export PATH="${HOME}"/opt/bin:"${PATH}"
@@ -25,8 +27,6 @@ export scratch="${SCRATCH}"
 # export MATLAB_USE_USERWORK=1
 
 # export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
-
-export npm_config_prefix="${HOME}"/.node_modules
 
 export QCPROGS="${HOME}"/opt/apps
 export apps="${QCPROGS}"
