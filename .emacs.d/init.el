2237ba6a326bf09d6d5c37652faa051baa97b353
Eric Berquist <eric.berquist@gmail.com>
2019-09-02 17:57:16 -0400

    [emacs] move all remaining bits from init.el to config.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,17 +1,1 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Load configuration
-
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; M-x customize
-
-;; Only package-selected-packages and custom-safe-themes should go in
-;; here.
-;; Links:
-;; https://www.reddit.com/r/emacs/comments/4x655n/packageselectedpackages_always_appear_after/
-;; https://www.reddit.com/r/emacs/comments/53zpv9/how_do_i_get_emacs_to_stop_adding_custom_fields/
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
