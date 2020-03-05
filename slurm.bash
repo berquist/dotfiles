518d2aedb0073c7c0ea797af5e4ee09693eca6ab
Eric Berquist <eric.berquist@gmail.com>
2020-03-05 15:26:53 -0500

    [shell] update SLURM stuff

--- slurm.bash
+++ slurm.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-format="\"%8i %9u %9P %35j %2t %10M %10l %5D %3C %R %H %I %J %z\""
+format="\"%8i %9u %9a %9P %9q %35j %2t %10M %10l %5D %3C %z %R\""
 flags="-S +i -o ${format}"
 alias q="squeue -u $USER ${flags}"
 alias qa="squeue -a ${flags}"
