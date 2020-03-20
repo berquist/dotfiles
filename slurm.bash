6a8f376f3abd660c2e5ef376f0960f70ac729ea3
Eric Berquist <berquist@isi.edu>
2020-03-20 15:07:00 -0700

    [shell] update SLURM stuff

--- slurm.bash
+++ slurm.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
+alias c="scontrol show job -d"
 format="\"%8i %9u %9a %9P %9q %35j %2t %10M %10l %5D %3C %z %R\""
 flags="-S +i -o ${format}"
 alias q="squeue -u $USER ${flags}"
 alias qa="squeue -a ${flags}"
-alias c="scontrol show job -d"
-alias walltimes='sacctmgr show qos'
 
 export SACCT_FORMAT=jobid,user,account,partition,jobname%30,state,exitcode,elapsed,nodelist,reqmem
+alias sl="sacct -a | less"
