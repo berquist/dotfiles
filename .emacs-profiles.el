c40e812739da800404bc5b1f93c6d8fd4efd8e19
Eric Berquist <eric.berquist@gmail.com>
2020-06-02 14:02:11 -0400

    [emacs] Update Emacs profile list

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -7,6 +7,8 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("elpy" . ((user-emacs-directory . "~/.emacs.d_elpy")))
  ("fresh" . ((user-emacs-directory . "~/.emacs.d_fresh")))
- ("straight" . ((user-emacs-directory . "~/.emacs.d_straight"))))
+ ("straight" . ((user-emacs-directory . "~/.emacs.d_straight")))
+ ("Phoenix181" . ((user-emacs-directory . "~/repositories/dotfiles-and-emacs-configs/dotfiles/Phoenix181/emacs/.emacs.d")))
+ ("tphmacs" . ((user-emacs-director . "~/development/speckledlemon/forks/tphmacs/.emacs.d"))))
 
 ;;; .emacs-profiles.el ends here
