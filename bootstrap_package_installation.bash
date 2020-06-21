5fa7908023a45b93d2d10f0a471acf940ed3688e
Eric Berquist <eric.berquist@gmail.com>
2020-06-20 20:29:56 -0400

    [deploy] add Homebrew setup to Ansible

--- bootstrap_package_installation.bash
+++ bootstrap_package_installation.bash
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-yay -S ansible
+funame=$(uname -a)
+base_plat=$(echo ${funame} | cut -d ' ' -f 1)
+
+case ${base_plat} in
+    # TODO don't assume that we're on Arch: add Pop!_OS detection
+    Linux)
+        sudo pacman -S ansible
+        ;;
+    Darwin)
+        if [[ ! -f /usr/local/bin/brew ]]; then
+            /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+        /usr/local/bin/brew install ansible
+        ;;
+    *)
+esac
+
 ansible-galaxy install -r requirements.yaml
--- install_linuxbrew.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-packages=(
-    fd
-    fzf
-)
