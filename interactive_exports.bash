a5607b4da07c7bf4d579efbd3e65c91ac2685365
Eric Berquist <eric.berquist@gmail.com>
2020-06-21 11:32:11 -0400

    [shell] pyenv now complains about virtualenv prompt mangling

--- interactive_exports.bash
+++ interactive_exports.bash
@@ -12,6 +12,7 @@ export LESSOPEN="|$(command -v lesspipe.sh) %s"
 export MANWIDTH=78
 
 # export PIP_REQUIRE_VIRTUALENV=1
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
 if [[ "$COLORTERM" == "xfce4-terminal" ]] && [[ "$TMUX" == "" ]] && [[ -e /usr/share/terminfo/x/xterm-256color ]]; then
     export TERM=xterm-256color
