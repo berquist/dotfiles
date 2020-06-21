d20bf1efbc97d8cbad86f17cd5b0efa688e4f6e6
Eric Berquist <eric.berquist@gmail.com>
2020-06-20 23:44:09 -0400

    [deploy] don't bother installing Ansible on Arch if it already exists

--- bootstrap_package_installation.bash
+++ bootstrap_package_installation.bash
@@ -8,7 +8,9 @@ base_plat=$(echo ${funame} | cut -d ' ' -f 1)
 case ${base_plat} in
     # TODO don't assume that we're on Arch: add Pop!_OS detection
     Linux)
-        sudo pacman -S ansible
+        if [[ ! -f /usr/bin/ansible ]]; then
+            sudo pacman -S ansible
+        fi
         ;;
     Darwin)
         if [[ ! -f /usr/local/bin/brew ]]; then
