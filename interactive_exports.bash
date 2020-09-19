cb9720b1fd60a97401ab7f640fc2f00328ee0273
Eric Berquist <eric.berquist@gmail.com>
2020-09-19 15:07:33 -0400

    [shell] disable lesspipe, it's being slow and not working

--- interactive_exports.bash
+++ interactive_exports.bash
@@ -5,9 +5,8 @@ export EDITOR="emacs -nw"
 # https://twitter.com/hhulkko/status/114256631419772928
 # http://darrendev.blogspot.com/2011/07/customize-less-less-annoying.html
 export LESS="-FRX"
-export LESS_ADVANCED_PREPROCESSOR=1
-# `brew install lesspipe`
-export LESSOPEN="|$(command -v lesspipe.sh) %s"
+# export LESS_ADVANCED_PREPROCESSOR=1
+# export LESSOPEN="|$(command -v lesspipe.sh) %s"
 
 export MANWIDTH=78
 
