2c1f693982ccd4e5a2adc8f0fbb494094028ee1b
Eric Berquist <eric.berquist@gmail.com>
2020-12-26 22:01:52 -0500

    [zsh] start gutting shell config

--- history.zsh
+++ history.zsh
@@ -5,8 +5,30 @@ export HISTCONTROL=
 export HISTFILESIZE=${_size}
 export HISTSIZE=${_size}
 export HISTFILE="${HOME}"/.zhistfile
-# Force prompt to write history after every command.
-# http://superuser.com/questions/20900/bash-history-loss
-PROMPT_COMMAND="history -a; ${PROMPT_COMMAND}"
 # Max number of history lines saved
 export SAVEHIST=${_size}
+
+## History command configuration
+setopt append_history
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
+
+
+function omz_history {
+  local list
+  zparseopts -E l=list
+
+  if [[ -n "$list" ]]; then
+    # if -l provided, run as if calling `fc' directly
+    builtin fc "$@"
+  else
+    # unless a number is provided, show all history events (starting from 1)
+    [[ ${@[-1]-} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
+  fi
+}
+
+alias history='omz_history -i'
