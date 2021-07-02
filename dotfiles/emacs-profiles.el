19db389e50a7e10143d6e1e5026b6009fc742670
Eric Berquist <eric.berquist@gmail.com>
2021-07-01 22:41:37 -0400

    [emacs] stop using symlink for emacs gitignore; remove straight testing profile

--- dotfiles/emacs-profiles.el
+++ dotfiles/emacs-profiles.el
@@ -7,7 +7,6 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("elpy" . ((user-emacs-directory . "~/.emacs.d_elpy")))
  ("fresh" . ((user-emacs-directory . "~/.emacs.d_fresh")))
- ("straight" . ((user-emacs-directory . "~/.emacs.d_straight")))
  ("Phoenix181" . ((user-emacs-directory . "~/repositories/dotfiles-and-emacs-configs/dotfiles/Phoenix181/emacs/.emacs.d")))
  ("tphmacs" . ((user-emacs-director . "~/development/speckledlemon/forks/tphmacs/.emacs.d"))))
 
