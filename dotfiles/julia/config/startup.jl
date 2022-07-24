7d8fa5dd5a57a08cc994cce0d3a2733f03cd891f
Eric Berquist <eric.berquist@gmail.com>
2022-07-24 12:59:20 -0400

    [deploy] initial Julia configuration

---
+++ dotfiles/julia/config/startup.jl
@@ -0,0 +1,30 @@
+# Adapted from https://discourse.julialang.org/t/what-is-in-your-startup-jl/18228
+
+import Pkg
+let
+    pkgs = ["Revise", "OhMyREPL", "PkgTemplates"]
+    for pkg in pkgs
+        if Base.find_package(pkg) === nothing
+            Pkg.add(pkg)
+        end
+    end
+end
+
+using Revise
+using OhMyREPL
+# TODO how to only have this in REPL?
+using PkgTemplates
+
+#atreplinit() do repl
+template = Template(;
+                    dir="~/development/julia",
+                    plugins=[
+                        License(; name="BSD3"),
+                        Git(; ssh=true),
+                        GitHubActions(),
+                        Codecov(),
+                        Coveralls(),
+                        Documenter{GitHubActions}(),
+                    ],
+                    )
+#end
