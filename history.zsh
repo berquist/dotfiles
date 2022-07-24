08fd87bdcf2cf86641f2857173f6f7ac8e854aae
Eric Berquist <eric.berquist@gmail.com>
2022-07-24 13:55:23 -0400

    [zsh] indentation

--- history.zsh
+++ history.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
 _size=1000000000
+# TODO:
+# - not sure these need to be exported
+# - HISTCONTROL?
 export HISTCONTROL=
 export HISTFILESIZE=${_size}
 export HISTSIZE=${_size}
@@ -19,16 +22,16 @@ setopt share_history          # share command history data
 
 
 function omz_history {
-  local list
-  zparseopts -E l=list
+    local list
+    zparseopts -E l=list
 
-  if [[ -n "$list" ]]; then
-    # if -l provided, run as if calling `fc' directly
-    builtin fc "$@"
-  else
-    # unless a number is provided, show all history events (starting from 1)
-    [[ ${@[-1]-} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
-  fi
+    if [[ -n "$list" ]]; then
+        # if -l provided, run as if calling `fc' directly
+        builtin fc "$@"
+    else
+        # unless a number is provided, show all history events (starting from 1)
+        [[ ${@[-1]-} = *[0-9]* ]] && builtin fc -l "$@" || builtin fc -l "$@" 1
+    fi
 }
 
 alias history='omz_history -i'
