52d0b8532815b0a26a1b25b243498c563755748a
Eric Berquist <eric.berquist@gmail.com>
2022-11-13 19:05:35 -0500

    [emacs] h5dump: start font locking

--- dotfiles/emacs.d/h5dump.el
+++ dotfiles/emacs.d/h5dump.el
@@ -1,5 +1,28 @@
 ;;; h5dump.el --- Navigate through h5dump output -*- lexical-binding: t -*-
 
+(require 'rx)
+
+;; TODO splice all these in
+;; TODO "HDF5" is like a singleton header
+;; TODO "SCALAR" is special
+(defconst h5dump--hdf5-keywords
+  '("ATTRIBUTE"
+    "DATA"
+    "DATASET"
+    "DATASPACE"
+    "DATATYPE"
+    "GROUP"))
+
+(defconst h5dump--hdf5-datatypes
+  '("H5T_STD_U8LE"
+    "H5T_STD_U32LE"))
+
+(defvar h5dump-font-lock-definitions
+  (append
+   `((,(rx symbol-start (or "H5T_STD_U8LE" "H5T_STD_U32LE") symbol-end) . font-lock-constant-face)
+     (,(rx symbol-start (or "ATTRIBUTE" "DATA" "DATASET" "DATASPACE" "DATATYPE" "GROUP") symbol-end) . font-lock-keyword-face)
+     (,(rx symbol-start (or "SCALAR") symbol-end) . font-lock-constant-face))))
+
 (define-derived-mode h5dump-mode prog-mode "h5dump"
   "Major mode for inspecting the raw output of h5dump."
   ;; Since this isn't really code, more like semi-structured text, there are
@@ -7,6 +30,7 @@
   ;; satisfy `hs-grok-mode-type'.
   (setq-local comment-start ""
               comment-end "")
+  (setq-local font-lock-defaults '(h5dump-font-lock-definitions))
   ;; entry syntax of `hs-special-modes-alist':
   ;; '(MODE
   ;;   START
