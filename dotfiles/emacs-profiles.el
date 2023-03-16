14863a8f0a682af25317fa60908b3faf2c180c06
Eric Berquist <eric.berquist@gmail.com>
2023-03-16 14:46:40 -0400

    [emacs] add eglot experiment

--- dotfiles/emacs-profiles.el
+++ dotfiles/emacs-profiles.el
@@ -7,6 +7,7 @@
 (("default" . ((user-emacs-directory . "~/dotfiles/dotfiles/emacs.d")))
  ("fresh" . ((user-emacs-directory . "~/dotfiles/dotfiles/emacs.d_fresh")))
  ("lsp" . ((user-emacs-directory . "~/dotfiles/dotfiles/emacs.d_lsp")))
+ ("eglot" . ((user-emacs-directory . "~/dotfiles/dotfiles/emacs.d_eglot")))
  ("Phoenix181" . ((user-emacs-directory . "~/repositories/dotfiles-and-emacs-configs/dotfiles/Phoenix181/emacs/.emacs.d")))
  ("tphmacs" . ((user-emacs-director . "~/development/speckledlemon/forks/tphmacs/.emacs.d"))))
 
