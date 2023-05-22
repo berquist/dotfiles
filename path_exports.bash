310a534c231d068a1d4ccea2853f550d7986b0bc
Eric Berquist <eric.berquist@gmail.com>
2023-05-22 00:07:48 -0400

    [zsh] "fix" pyenv-virtualenv

--- path_exports.bash
+++ path_exports.bash
@@ -26,14 +26,13 @@ prepend_to_path "${HOME}/.cargo/bin"
 prepend_to_path "${HOME}/.nimble/bin"
 prepend_to_path "${HOME}/.juliaup/bin"
 
+source "${HOME}"/dotfiles/pyenv.bash
+
 export SCRATCH=/tmp
 export scratch="${SCRATCH}"
 
 export QCPROGS="${HOME}"/opt/apps
 export apps="${QCPROGS}"
-
-if command -v pyenv >/dev/null 2>&1; then eval "$(pyenv init -)"; fi
-if command -v pyenv-virtualenv-init 1>/dev/null 2>&1; then eval "$(pyenv virtualenv-init -)"; fi
 
 hostname=$(hostname)
 
