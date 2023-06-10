d9686cf0abd0501debb7ed96038752a4f21fcf50
Eric Berquist <eric.berquist@gmail.com>
2023-06-10 00:02:18 -0400

    [emacs] try switching from straight to elpaca

--- dotfiles/emacs.d/early-init.el
+++ dotfiles/emacs.d/early-init.el
@@ -2,3 +2,10 @@
 
 (setq gc-cons-threshold (* 1024 1024 100))
 (set-language-environment "UTF-8")
+(setq package-enable-at-startup nil)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-native-compile: t
+;; no-update-autoloads: t
+;; End:
