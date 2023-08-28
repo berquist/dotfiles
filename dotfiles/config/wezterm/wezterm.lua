2e58c9368f99b5528a5f6f4f3d659125155b8d36
Eric Berquist <eric.berquist@gmail.com>
2023-08-28 09:39:32 -0400

    [wezterm] use wombat color theme

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -6,7 +6,9 @@ if wezterm.config_builder then
    config = wezterm.config_builder()
 end
 
+config.color_scheme = 'Wombat'
 config.font = wezterm.font 'Panic Sans'
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+-- config.window_background_opacity = 0.75
 
 return config
