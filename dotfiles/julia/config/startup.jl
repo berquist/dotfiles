9d502a093bde55481391718fdbcaf0a1fbffdec6
Eric Berquist <eric.berquist@gmail.com>
2023-09-10 23:12:15 -0400

    [Julia] update startup.jl

--- dotfiles/julia/config/startup.jl
+++ dotfiles/julia/config/startup.jl
@@ -1,35 +1,55 @@
 # Adapted from https://discourse.julialang.org/t/what-is-in-your-startup-jl/18228
 
-import Pkg
-let
+import Pkg as var"#Pkg" # Don't import globally
+
+@async let
     pkgs = [
+        "AbbreviatedStackTraces",
         "Debugger",
         "Revise",
         "OhMyREPL",
         "PkgTemplates"
     ]
     for pkg in pkgs
-        if Base.find_package(pkg) === nothing
-            Pkg.add(pkg)
+        if isnothing(Base.find_package(pkg))
+            var"#Pkg".add(pkg)
         end
+    end
+    @eval using Revise
+    for pkg in pkgs
+        Core.eval(Main, :(using $(Symbol(pkg))))
     end
 end
 
-using Revise
-using OhMyREPL
-# TODO how to only have this in REPL?
-using PkgTemplates
-
-#atreplinit() do repl
-template = Template(;
-                    dir="~/development/julia",
-                    plugins=[
-                        License(; name="BSD3"),
-                        Git(; ssh=true),
-                        GitHubActions(),
-                        Codecov(),
-                        Coveralls(),
-                        Documenter{GitHubActions}(),
-                    ],
-                    )
-#end
+function template()
+    @eval begin
+        using PkgTemplates
+        Template(;
+            dir="~/development/julia",
+            plugins=[
+                Tests(; aqua=true),
+                License(; name="BSD3"),
+                Git(;
+                    manifest=true,
+                    ssh=true
+                ),
+                CompatHelper(),
+                TagBot(),
+                GitHubActions(;
+                    osx=true,
+                    windows=true,
+                    extra_versions=["1.6", "1.9", "nightly"],
+                ),
+                Codecov(),
+                Coveralls(),
+                Documenter{GitHubActions}(),
+                BlueStyleBadge(),
+                ColPracBadge(),
+                PkgEvalBadge(),
+                Dependabot(),
+                Citation(),
+                RegisterAction(),
+            ],
+        )
+    end
+end
