755918ed2b5daf1e616b077251f932baab7ba1e1
Eric Berquist <eric.berquist@gmail.com>
2023-09-14 15:18:53 -0600

    [shell] fix SPACK_ROOT export

--- path_exports.bash
+++ path_exports.bash
@@ -50,7 +50,7 @@ elif [[ "${hostname}" == "s1096537" ]]; then
     export SPACK_ROOT="${HOME}"/development/forks/spack
 else
     export PRE_COMMIT_HOME="${HOME}"/.cache/pre-commit
-    export SPACK_ROOT="${HOME}"/repositories/spack
+    [[ -n "${SPACK_ROOT}" ]] || export SPACK_ROOT="${HOME}"/repositories/spack
     export VAGRANT_HOME="${HOME}"/.vagrant.d
 fi
 
