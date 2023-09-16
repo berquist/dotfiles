026831153e55aa8b6f0e99cc8170dfa37884a0dc
Eric Berquist <eric.berquist@gmail.com>
2023-09-16 15:13:43 -0400

    [wezterm] fallback fonts

--- dotfiles/config/wezterm/wezterm.lua
+++ dotfiles/config/wezterm/wezterm.lua
@@ -9,7 +9,13 @@ end
 config.audible_bell = "Disabled"
 
 config.color_scheme = 'Wombat'
-config.font = wezterm.font 'Panic Sans'
+config.font = wezterm.font_with_fallback {
+   { family = 'Comic Mono' },
+   { family = 'Panic Sans' },
+   { family = 'Fira Mono' },
+}
+-- https://learn.microsoft.com/en-us/typography/opentype/spec/featuretags
+-- no contextual alternates, contextual ligatures, or standard ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 -- config.window_background_opacity = 0.75
 
