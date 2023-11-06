aaac5912946eb8d9d5d95ccb309309ae33a97333
Eric Berquist <eric.berquist@gmail.com>
2023-11-05 20:09:22 -0500

    [emacs] switch from setq{,-default} to setopt and :custom

--- dotfiles/emacs.d/early-init.el
+++ dotfiles/emacs.d/early-init.el
@@ -1,5 +1,9 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
+;; TODO look for a backport instead?
+(when (not (macrop 'setopt))
+  (defalias 'setopt 'setq-default))
+
 (setq gc-cons-threshold (* 1024 1024 100))
 (set-language-environment "UTF-8")
 (setq package-enable-at-startup nil)
