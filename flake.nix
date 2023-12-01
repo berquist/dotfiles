8c699e9bf1dfa73d62fdbdc645569f2c030997ed
Eric Berquist <eric.berquist@gmail.com>
2023-11-30 21:26:35 -0500

    [Nix] update flake nixpkgs to 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
