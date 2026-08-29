50d65dcefa0ba57b66b107153f919184b5b9b72e
Eric Berquist <eric.berquist@gmail.com>
2026-08-29 12:49:16 -0400

    [shell] gpg fixes

--- interactive_exports.bash
+++ interactive_exports.bash
@@ -27,3 +27,6 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 if command -v virtualenvwrapper_lazy.sh > /dev/null 2>&1; then
     source "$(command -v virtualenvwrapper_lazy.sh)"
 fi
+
+GPG_TTY="$(tty)"
+export GPG_TTY
